39aa16db2f88519120ef7a137bf603c2a2501c6c
heespari <heta.e.parikka@student.jyu.fi>
2025-09-15 21:17:37 +0300

    1.4

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -7,59 +7,57 @@ const Header = (props) => {
   )
 }
 
-const Content = ({part1, part2, part3}) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part name={part1.name} exercises={part1.exercises}/>
-      <Part name={part2.name} exercises={part2.exercises}/>
-      <Part name={part3.name} exercises={part3.exercises}/>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
     </div>
   )
 }
 
 const Part = (props) => {
-  let name = props.name;
-  let exercises = props.exercises
   return (
     <div>
       <p>
-        {name}, exercises: {exercises}
+        {props.name}, exercises: {props.exercises}
       </p>
     </div>
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
   return (
     <div>
-      <p>Total {props.total}
+      <p>Total {parts[0].exercises + parts[1].exercises + parts[2].exercises}
       </p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+ const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course} />
-      <Content 
-        part1={part1} part2={part2} part3={part3}
-      />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
